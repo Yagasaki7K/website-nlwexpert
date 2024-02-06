1d93455340d48f0ffebd6ee273b69f555f9dfd6d
Anderson Marlon <anderson18.marlon@gmail.com>
2024-02-06 12:29:47 -0300

    Finished: Day 02 - React

--- react/src/components/note-card.tsx
+++ react/src/components/note-card.tsx
@@ -1,12 +1,44 @@
-const NoteCard = () => {
-    return (
-        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 outline-none overflow-hidden relative 
-        hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-            <p className="text-sm leading-6 text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, sunt soluta! Nobis corrupti, quam ut explicabo nisi consectetur dignissimos sapiente deserunt dolorum ratione laboriosam assumenda dolorem pariatur officiis et placeat?</p>
+import * as Dialog from '@radix-ui/themes'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+interface NoteCardProps {
+    note: {
+        date: Date
+        content: string
+    }
+}
+
+const NoteCard = ({ note }: NoteCardProps) => {
+    return (
+        <Dialog.DialogRoot>
+            <Dialog.DialogTrigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 outline-none overflow-hidden relative 
+        hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+                <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            </Dialog.DialogTrigger>
+
+            <Dialog.Portal>
+                <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+                <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+                max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none h-[60vh]">
+                    <Dialog.DialogClose className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                        <X className="size-5" />
+                    </Dialog.DialogClose>
+                    <div className="flex flex-1 flex-col gap-3 p-5">
+                        <span className="text-sm font-medium text-slate-300">{formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}</span>
+                        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+                    </div>
+
+                    <button type="button" className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group">
+                        Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>
+                    </button>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.DialogRoot>
     )
 }
 
